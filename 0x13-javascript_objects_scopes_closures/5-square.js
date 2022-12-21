d4f5a61dc44f75f0cd744f4a02803199972b26d1
lanreoyekanmi <olanrewajuoyekanmi241@gmail.com>
2022-12-21 01:22:23 +0000

    rect

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,29 +1,6 @@
 #!/usr/bin/node
-// JS script to define a class, extends to another class
-class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      return this;
-    } else {
-      this.width = w;
-      this.height = h;
-    }
-  }
-  print () {
-    for (let count = 0; count < this.height; count++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-  rotate () {
-    let holder = this.width;
-    this.width = this.height;
-    this.height = holder;
-  }
-  double () {
-    this.width = 2 * this.width;
-    this.height = 2 * this.height;
-  }
-}
+
+const Rectangle = require('./4-rectangle.js');
 
 class Square extends Rectangle {
   constructor (size) {
