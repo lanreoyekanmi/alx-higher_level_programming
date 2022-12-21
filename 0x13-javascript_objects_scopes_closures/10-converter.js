d3987410318a27451e3ab11db246d17d99c934d8
lanreoyekanmi <olanrewajuoyekanmi241@gmail.com>
2022-12-21 00:53:33 +0000

    rect

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  function convert (number) {
-    return number.toString(base);
-  }
+  return function (num) {
+    return (num.toString(base));
+  };
+};
   
